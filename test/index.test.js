93be04191ed476000f00162a6275466e0a299a25
Miroslav Popov <info@forexsb.com>
2020-08-24 07:23:40 +0000

    Updated @popovmp/micro-tester.

--- test/index.test.js
+++ test/index.test.js
@@ -1,53 +1,54 @@
 "use strict"
 
-const {init, test, done} = require("@popovmp/micro-tester");
+const assert = require("assert");
+const {init, test, ensure} = require("@popovmp/micro-tester");
 
 const config = require("../index.js");
 config.init(__dirname);
 
 init("Test config-json");
 
-test("Have an `init` function", () =>
-    typeof config.init === "function"
-);
+test("Have an `init` function", () => {
+    assert.strictEqual(typeof config.init, "function");
+});
 
-test("Have a `get` function", () =>
-    typeof config.get === "function"
-);
+test("Have a `get` function", () => {
+    assert.strictEqual(typeof config.get, "function");
+});
 
-test("Get a string", () =>
-    config.get("key") === "value"
-);
+test("Get a string", () => {
+    assert.strictEqual(config.get("key"), "value");
+});
 
-test("Get a number", () =>
-    config.get("answer") === 42
-);
+test("Get a number", () => {
+    assert.strictEqual(config.get("answer"), 42);
+});
 
-test("Get true", () =>
-    config.get("yes") === true
-);
+test("Get true", () => {
+    assert.strictEqual(config.get("yes"), true);
+});
 
-test("Get false", () =>
-    config.get("no") === false
-);
+test("Get false", () => {
+    assert.strictEqual(config.get("no"), false);
+});
 
-test("Get null", () =>
-    config.get("nil") === null
-);
+test("Get null", () => {
+    assert.strictEqual(config.get("nil"), null);
+});
 
-test("Get a copy of an array", () =>
-    config.get("arr")[0] = 54 &&
-    config.get("arr")[0] === 3
-);
+test("Get a copy of an array", () => {
+    config.get("arr")[0] = 54;
+    assert.strictEqual(config.get("arr")[0], 3);
+});
 
-test("Get a copy of an object", () =>
-    config.get("obj")["a"] = 54 &&
-    config.get("obj")["a"] === 3
-);
+test("Get a copy of an object", () => {
+    config.get("obj")["a"] = 54;
+    assert.strictEqual(config.get("obj")["a"], 3);
+});
 
-test("Get a missing value", () =>
+test("Get a missing value", () => {
     // It prints an error to the console and returns undefined.
-    config.get("foo") === undefined
-);
+    assert.strictEqual(config.get("foo"), undefined);
+});
 
-done();
+ensure();
